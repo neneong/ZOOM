ce8c441b39dbacca53747053bb8ba729a2bdd8a6
neneong <pukkuk12@naver.com>
2022-07-26 13:36:55 +0900

    second

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express, { application } from "express"
 
 const app = express();
 
-app.listen(3000);
+app.set('view engine', 'pug');
+app.set('views', __dirname + "/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (req, res)=>res.render("home"));
 
-console.log("hello");
+const handleListen = () => console.log("LISTENING ON http://localhost:3000")
+
+app.listen(3000, handleListen);
